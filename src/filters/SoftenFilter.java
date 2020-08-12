3511378709a92077544b7a42dd2a3198d31896c9
Chinenye <Stellaezenwoye@gmail.com>
2020-08-12 03:08:23 -0700

    soften filter class
    i did an extrat constant for the -1 to min_int

--- src/filters/SoftenFilter.java
+++ src/filters/SoftenFilter.java
@@ -13,16 +13,19 @@ package filters;
  * @version 1.1
  */
 public class SoftenFilter extends AbstractWeightedFilter {
+    //Stella: extracting a constant changing the -1 into min_int
+    //        4 into max_int and 1 
+    private static final int MIN_INT = 1;
     /**
      * A 3x3 grid of weights to use for the weighted filter algorithm.
      */
-    private static final int[][] WEIGHTS = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
+    private static final int[][] theWEIGTH = {{MIN_INT, 2, 1}, {2, 4, 2}, {1, 2, 1}};
 
     /**
      * Constructs a new softening filter.
      */
     public SoftenFilter() {
-        super("Soften", WEIGHTS);
+        super("Soften", theWEIGTH);
     }
 
 }
