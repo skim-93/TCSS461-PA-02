57c9b4c6bf694fa87ccb61da9838fac3febb5693
Chinenye <Stellaezenwoye@gmail.com>
2020-08-12 04:24:05 -0700

    refactor
    did refactor methods:
    extract a constant
    pull-up method
    extract local variables
    inline code
    rename field

--- src/filters/EdgeDetectFilter.java
+++ src/filters/EdgeDetectFilter.java
@@ -15,10 +15,8 @@ import image.PixelImage;
  * @version 1.1
  */
 public class EdgeDetectFilter extends AbstractFilter {
-    /**
-     * A 3x3 matrix of weights to use in the filtering algorithm.
-     */
-    private static final int[][] WEIGHTS = {{-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}};
+    //Stella: extracting a constant changing the -1 into min_int
+    private static final int[][] WEIGHTS = {{MIN_INT, MIN_INT, MIN_INT}, {MIN_INT, 8, MIN_INT}, {MIN_INT, MIN_INT, MIN_INT}};
 
     /**
      * Constructs a new edge detection filter.
@@ -32,6 +30,7 @@ public class EdgeDetectFilter extends AbstractFilter {
      * 
      * @param theImage The image.
      */
+    //Refactor Change Function Declaration (Seungku Kim)
     @Override
     public void filter(final PixelImage theImage) {
         weight(theImage, WEIGHTS);
