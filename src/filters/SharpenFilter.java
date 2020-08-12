dcc4b9f56485f7fbab05a623c273060aa700a22d
Chinenye <Stellaezenwoye@gmail.com>
2020-08-12 02:57:14 -0700

    fetched the original copy
    start refactoring added extract a constant for -1 since it is constant throught out the code for SharpenFilter class

--- src/filters/SharpenFilter.java
+++ src/filters/SharpenFilter.java
@@ -13,10 +13,12 @@ package filters;
  * @version 1.1
  */
 public class SharpenFilter extends AbstractWeightedFilter {
+    //Stella: extracting a constant changing the -1 into min_int
+    private static final int MIN_INT = -1;
     /**
      * A 3x3 grid of weights to use for the weighted filter algorithm.
      */
-    private static final int[][] WEIGHTS = {{-1, -2, -1}, {-2, 28, -2}, {-1, -2, -1}};
+    private static final int[][] WEIGHTS = {{MIN_INT, -2, MIN_INT}, {-2, 28, -2}, {MIN_INT, -2, MIN_INT}};
 
     /**
      * Constructs a new sharpening filter.
